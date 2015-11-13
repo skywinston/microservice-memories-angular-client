93566c2e428bb551963b1a37cdb1e09fa36d1fd7
Sky Winston <skywinston@gmail.com>
2015-11-13 16:31:22 -0700

    added service registry support

--- controllers/MemoriesCtrl.js
+++ controllers/MemoriesCtrl.js
@@ -1,27 +1,10 @@
 app.controller('MemoriesCtrl', function MemoriesCtrl($scope, $http, $routeParams){
 
-  $scope.getMemories = function(){
-    $http.get('http://g12-sky-winston-memories.cfapps.io/api/v1/memories').success(function(response){
-      $scope.memories = response.data;
-    });
-  }
+  var serviceRegistryUrl = 'http://galvanize-service-registry.cfapps.io/api/v1/cohorts/g12';
+  var guarantee = '/kids-these-days?guarantee='
+  var myRegistry = 'http://g12-sky-winston-memories.cfapps.io/api/v1/memories';
 
-  $scope.getYears = function(){
-    $http.get('http://g12-sky-winston-memories.cfapps.io/api/v1/memories/years').success(function(response){
-      $scope.years = response.data;
-    });
-  }
-
-  $scope.yearLookup = function(year){
-    $http.get('http://g12-sky-winston-memories.cfapps.io/api/v1/memories/'+year).success(function(response){
-      $scope.memories = response.data;
-    });
-  }
-
-  $scope.getMemories();
-  $scope.getYears();
-
-  $scope.newMemory = {
+  var defaultMemory = {
     data: {
       type: "memory",
       attributes: {
@@ -30,12 +13,41 @@ app.controller('MemoriesCtrl', function MemoriesCtrl($scope, $http, $routeParams
         "year": ''
       }
     }
-  }
+  };
 
-  $scope.addMemory = function(){
-    $http.post('http://g12-sky-winston-memories.cfapps.io/api/v1/memories', $scope.newMemory).success(function(response){
-      console.log(response);
-      $scope.getMemories();
+  $scope.memoriesUrl = '';
+  $scope.getMemories = function(){
+    $http.get(serviceRegistryUrl + guarantee + myRegistry).success(function(response){
+      $scope.memoriesUrl = response.data[0].attributes.url;
+      $http.get($scope.memoriesUrl).success(function(response){
+        $scope.memories = response.data;
+        $scope.getYears();
+      });
     });
   }
+
+  $scope.getYears = function(){
+    $http.get($scope.memoriesUrl + '/years').success(function(response){
+      $scope.years = response.data;
+    });
+  }
+
+  $scope.yearLookup = function(year){
+    $http.get($scope.memoriesUrl + '/' + year).success(function(response){
+      $scope.memories = response.data;
+    });
+  }
+
+  $scope.getMemories();
+
+  $scope.addMemory = function(){
+    $http.post(memoriesUrl, $scope.newMemory).success(function(response){
+      $scope.getMemories();
+      $scope.newMemory = defaultMemory;
+    });
+  }
+
+  $scope.newMemory = defaultMemory;
+
+
 });
